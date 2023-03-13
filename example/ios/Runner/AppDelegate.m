77651baf65b2adbd85f350546986126ff6a1e698
李雪健 <18519100241@163.com>
2023-03-13 12:43:50 +0800

    Limit Orientation

--- example/ios/Runner/AppDelegate.m
+++ example/ios/Runner/AppDelegate.m
@@ -1,5 +1,10 @@
 #import "AppDelegate.h"
 #import "GeneratedPluginRegistrant.h"
+#if __has_include(<ios_orientation/IosOrientationPlugin.h>)
+#import <ios_orientation/IosOrientationPlugin.h>
+#else
+@import ios_orientation;
+#endif
 
 @implementation AppDelegate
 
@@ -8,6 +13,14 @@
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+- (UIInterfaceOrientationMask)application:(UIApplication *)application supportedInterfaceOrientationsForWindow:(UIWindow *)window {
+    
+    if (IosOrientationPlugin.portrait) {
+        return UIInterfaceOrientationMaskPortrait;
+    }
+    return UIInterfaceOrientationMaskAllButUpsideDown;
 }
 
 @end
